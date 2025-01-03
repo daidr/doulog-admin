d536a1adfe09d72fab4fce064ea9f6528f156224
daidr <daidr@daidr.me>
2025-01-04 02:20:15 +0800

    feat: update ghost button style

--- src/components/base/BaseButton.vue
+++ src/components/base/BaseButton.vue
@@ -52,19 +52,8 @@ button {
     @apply text-gray-500 relative;
     @apply bg-transparent;
 
-    &::before {
-      @apply content-empty bg-current;
-      @apply absolute bottom-0 left-1.5 right-1.5 h-2px;
-      @apply transform-gpu scale-x-1 opacity-0 rounded-xl duration-300;
-      transition-property: opacity, transform;
-    }
-
     &:not(:disabled):hover {
       @apply bg-gray-200;
-
-      &::before {
-        @apply scale-x-100 opacity-50;
-      }
     }
 
     &:not(:disabled):active {
